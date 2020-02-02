0930957fe2c450c7087ee5280cf7e22c975b008b
Josh <joshmartin5001@gmail.com>
2020-02-01 22:23:11 -0500

    Updates

--- src/Display.py
+++ src/Display.py
@@ -16,15 +16,17 @@ class Display:
         self.image_black = None
         self.draw_black = None
 
-        self.image_red =  None
-        self.draw_red =  None
+        self.image_red = None
+        self.draw_red = None
+        self.update_moment = time.strftime("%I") + ':' + time.strftime("%M") + ' ' + time.strftime("%p")
+
         self.fonts = Fonts()
 
         self.reset_screen()
-    
+
     def header_title(self, title: str) -> None:
-        self.draw_red.rectangle((245,0, 640, 55), fill = 0) # Task area banner
-        self.draw_red.text((250,10), title, font = self.fonts.tasks_list_title, fill = 255) # Task text
+        self.draw_red.rectangle((245, 0, 640, 55), fill=0)  # Task area banner
+        self.draw_red.text((250, 10), title, font=self.fonts.tasks_list_title, fill=255)  # Task text
 
     def reset_screen(self):
         images_size = (self.width, self.height)
@@ -34,11 +36,15 @@ class Display:
         self.image_red = Image.new('1', images_size, (1))
         self.draw_red = ImageDraw.Draw(self.image_red)
 
-        self.draw_black.rectangle((0,0,240, 384), fill = 0) # Calender area rectangle
-        self.draw_black.line((10,320,230,320), fill = 255) # Weather line
-        self.draw_black.line((250,320,640,320), fill = 0) # Footer for additional items
-        self.draw_red.rectangle((245,0, 640, 55), fill = 0) # Task area banner
-        #self.draw_black.text((585,370),update_moment,font = font_update_moment, fill = 255) # The update moment in Pooch
+        self.draw_black.rectangle((0, 0, 240, 384), fill=0)  # Calender area rectangle
+        self.draw_black.line((10, 320, 230, 320), fill=255)  # Weather line
+        self.draw_black.line((250, 320, 640, 320), fill=0)  # Footer for additional items
+        self.draw_red.rectangle((245, 0, 640, 55), fill=0)  # Task area banner
+
+        self.update_moment = time.strftime("%I") + ':' + time.strftime("%M") + ' ' + time.strftime("%p")
+        self.draw_black.text((585, 370), self.update_moment, font=self.fonts.update_moment,
+                             fill=255)  # The update moment in Pooch
+
     def refresh(self):
         logging.info("Display:  Refreshing on A {} Display".format(self.type_of_display))
         self.show()
